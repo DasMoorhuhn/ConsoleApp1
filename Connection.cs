5f903f7a7ef6c3f8caafeaff37220a98ff268bca
Hendrik <Hendrik@DESKTOP-OH054JV>
2021-07-07 21:16:35 +0200

    Add project files.

---
+++ Connection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDP
+{
+	class Connection
+	{
+		byte[] sendbuf;
+		string ipAddr;
+		int port;
+		IPAddress broadcast;
+		IPEndPoint ep;
+		Socket s;
+
+		public Connection(string _ip, int _port)
+		{
+			this.ipAddr = _ip;
+			this.port = _port;
+			this.s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+			this.broadcast = IPAddress.Parse(this.ipAddr);
+			this.ep = new IPEndPoint(broadcast, this.port);
+		}
+
+		public void sendPackage(string message)
+		{
+			this.sendbuf = Encoding.ASCII.GetBytes(message);
+			s.SendTo(sendbuf, ep);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace UDP
+{
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Connection connection = new Connection("192.168.178.51", 9999);
+      connection.sendPackage("CMD;A001;LED_01;OFF");
+    }
+  }
+}
